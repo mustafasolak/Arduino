81a8de7777a4e23e7340ec66995f060c03909646
Mustafa SOLAK <mustafasolak22@hotmail.com>
2021-06-10 09:43:57 +0300

    Update 1-LedBlink.ino

--- 1-LedExample/1-LedBlink.ino
+++ 1-LedExample/1-LedBlink.ino
@@ -1,14 +1,14 @@
-
+#define led 13
 
 void setup() {
 
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(led, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
 }
